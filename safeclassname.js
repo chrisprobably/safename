b9ddf04f7e752ac9499e7108027b42c3a64c44a8
Christopher Clarke <chris.clarke@caplin.com>
2017-08-18 12:12:41 +0100

    Triple equals

--- safeclassname.js
+++ safeclassname.js
@@ -3,7 +3,7 @@ const SPACE = 32;
 
 const safeclassname = str => str.replace(/[^A-Za-z0-9]/g, s => {
     const c = s.charCodeAt(0);
-    if (c == SPACE) return '-';
+    if (c === SPACE) return '-';
     return '_x' + c.toString(16);
 });
 
